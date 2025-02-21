36ff84df7433366bed5a9ce753a9515314c86579
Deven <devhfos@gmail.com>
2025-02-21 15:40:35 +0000

    add function to calculate the frequency of characters in a given book

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = open_book(book_path)
     num_words = count_words(book_text)
+    char_frequency = calc_char_frequency(book_text)
     print(book_text)
     print(f"This book has {num_words} words.")
+    print(char_frequency)
 
 def open_book(path):
     with open(path) as f:
@@ -13,5 +15,15 @@ def open_book(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def calc_char_frequency(text):
+    dict = {}
+    for char in text.lower():
+        keys = dict.keys()
+        if char in keys:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
 
 main()
