22f96eb3eae2ba7ee48d4da5cffe5508b6d7e17b
Deven <devhfos@gmail.com>
2025-02-21 19:10:41 +0000

    clean up commented out lines

--- main.py
+++ main.py
@@ -10,15 +10,7 @@ def main():
     book_path = sys.argv[1]
     book_text = open_book(book_path)
     num_words = count_words(book_text)
-    char_frequency = calc_char_frequency(book_text)
-    # print(book_text)
-    #print(f"{num_words} words found in the document")
-    #print(char_frequency)
-    # print(list(char_frequency.items()).sort(reverse=True, key=sort_on))
-    #print(list(char_frequency.items()))
-    #print(sort_chars_by_freq(char_frequency))
-    #print(sort_chars_by_freq(char_frequency))
-    
+    char_frequency = calc_char_frequency(book_text)   
      
     create_report(sort_chars_by_freq(char_frequency), book_path, num_words)
 
@@ -36,7 +28,6 @@ def create_report(sorted_list, book_path, num_words):
     for i in sorted_list:
         char_data = list(i.values())
         print(f"{char_data[0]}: {char_data[1]}")
-        #print(f"The '{char_data[0]}' character was found {char_data[1]} times")
     print("============= END ===============")
 
 
