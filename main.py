d227d78302946efeeb85fa1716d08c61763d3235
Deven <devhfos@gmail.com>
2025-02-21 14:56:10 +0000

    clean up comments

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ def main():
     num_words = count_words(book_text)
     print(book_text)
     print(f"This book has {num_words} words.")
-    
-    #with open("books/frankenstein.txt") as f:
-    #    file_contents = f.read()
-    #    print(file_contents)
-    #    print(count_words(file_contents))
 
 def open_book(path):
     with open(path) as f:
